1264eeadd6fccd2e5a3bc9d564c4769781cf12c9
Le Truong An <anlt333@gmail.com>
2023-06-22 08:23:53 +0000

    a

--- ftp/ad-disable-user.py
+++ ftp/ad-disable-user.py
@@ -88,10 +88,73 @@ def disable_account_1(action=None, success=None, container=None, results=None, h
     ## Custom Code End
     ################################################################################
 
-    phantom.act("disable account", parameters=parameters, name="disable_account_1", assets=["adldap-defenders"], callback=post_data_1)
+    phantom.act("disable account", parameters=parameters, name="disable_account_1", assets=["adldap-defenders"], callback=decision_1)
 
     return
 
+
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_1() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["action_result.status", "==", "success"]
+        ])
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        post_data_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'else' condition 2
+    post_data_2(action=action, success=success, container=container, results=results, handle=handle)
+
+    return
+
+def post_data_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("post_data_2() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    container_artifact_data_userName = phantom.collect2(container=container, datapath=["artifact:*.cef.destinationUserName","artifact:*.id"])
+    container_artifact_data_host = phantom.collect2(container=container, datapath=["artifact:*.cef.deviceCustomString1","artifact:*.id"])
+    body_formatted_string = phantom.format(
+        container=container,
+        template="""{\n  \"user\": \"False\",\n  \"host\": \"%s\",\n  \"status\": 0,\n  \"description\": \"string\"\n}"""%(container_artifact_data_host[0][0]),
+        parameters=[])
+    headers_formatted_string = phantom.format(
+        container=container,
+        template="""{\n\"Content-Type\": \"application/json\",\n\"accept\":\"application/json\"\n}""",
+        parameters=[])
+    location_formatted_string = phantom.format(
+        container=container,
+        template="""/alert""",
+        parameters=[])
+
+    parameters = []
+
+    if body_formatted_string is not None and location_formatted_string is not None:
+        parameters.append({
+            "body": body_formatted_string,
+            "headers": headers_formatted_string,
+            "location": location_formatted_string,
+            "verify_certificate": False,
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("post data", parameters=parameters, name="post_data_2", assets=["notification-api"])
+
+    return
 
 def on_finish(container, summary):
     phantom.debug("on_finish() called")
